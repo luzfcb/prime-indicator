8a71062b009085566466f1ebbe912b42151fb6fe
Matthías Páll Gissurarson <mpg@mpg.is>
2014-06-15 03:33:31 +0000

    fixed some indent

--- remove.sh
+++ remove.sh
@@ -4,16 +4,16 @@ function removePrime    {
     ps aux | grep /usr/bin/prime-indicator | grep -v grep | awk '{print $2}' | xargs kill
     rm /usr/bin/prime-indicator
     rm -r /usr/lib/primeindicator/
-	rm /etc/sudoers.d/prime-indicator-sudoers
+    rm /etc/sudoers.d/prime-indicator-sudoers
 
 }
 if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run using sudo" 2>&1
-	exit 1
+    echo "This script must be run using sudo" 2>&1
+    exit 1
 else
-	read -n1 -p "Remove PRIME Indicator? (y/N) "
-	echo
-	if [[ $REPLY = [yY] ]]; then
+    read -n1 -p "Remove PRIME Indicator? (y/N) "
+    echo
+    if [[ $REPLY = [yY] ]]; then
        removePrime
     fi
 fi
