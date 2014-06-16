bb1249ab810b3587a1e647f75881c2f037b46373
Matthías Páll Gissurarson <mpg@mpg.is>
2014-06-16 00:51:11 +0000

    fixed remove and setup

--- remove.sh
+++ remove.sh
@@ -2,9 +2,10 @@
 function removePrimeIndicator    {
     #Kill running prime-indicator
     ps aux | grep /usr/bin/prime-indicator | grep -v grep | awk '{print $2}' | xargs kill
-    rm /usr/bin/prime-indicator
-    rm -r /usr/lib/primeindicator/
-    rm /etc/sudoers.d/prime-indicator-sudoers
+    rm -f /usr/bin/prime-indicator
+    rm -rf /usr/lib/primeindicator/
+    rm -f /etc/sudoers.d/prime-indicator-sudoers
+    rm -f $HOME/.config/autostart/prime-indicator.desktop
 
 }
 if [[ $EUID -ne 0 ]]; then
